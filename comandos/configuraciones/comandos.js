d980d0c137a01ffa9352addafb2ea3e2f18ab3c0
shxdowlynnnjade <tvxuperlove@gmail.com>
2026-03-06 18:34:48 -0300

    Actualizar comandos.js

--- comandos/configuraciones/comandos.js
+++ comandos/configuraciones/comandos.js
@@ -5,36 +5,37 @@ import syntaxerror from 'syntax-error'
 import importFile from './import.js'
 import Helper from './manejador.js'
 
-const __dirname = manejador.__dirname(import.meta)
-const manejadorFolder = manejador.__dirname(join(__dirname, './comandos/index'))
+const __dirname = Helper.__dirname(import.meta)
+const comandosFolder = Helper.__dirname(join(__dirname, './comandos'))
 const comandosFilter = filename => /\.(mc)?js$/.test(filename)
 
+let watcher = {}
+let comandos = {}
+let comandosFolders = []
 
-let watcher, manejador, comandosFolders = []
-watcher = comandos = {}
+async function filesInit(folder = comandosFolder, filter = comandosFilter, conn) {
+    const resolved = resolve(folder)
+    if (resolved in watcher) return
+    comandosFolders.push(resolved)
 
-async function filesInit(comandosFolder = comandosFolder, comandosFilter = comandosFilter, conn) {
-    const folder = resolve(comandosFolder)
-    if (folder in watcher) return
-    comandosFolders.push(folder)
+    await Promise.all(
+        readdirSync(resolved).filter(filter).map(async filename => {
+            try {
+                let file = globalThis.__filename(join(resolved, filename))
+                const module = await import(file)
+                if (module) comandos[filename] = module.default || module
+            } catch (e) {
+                conn?.logger.error(e)
+                delete comandos[filename]
+            }
+        })
+    )
 
-    await Promise.all(readdirSync(folder).filter(comandosFilter).map(async filename => {
-        try {
-            let file = globalThis.__filename(join(folder, filename))
-            const module = await import(file)
-            if (module) comandos[filename] = 'default' in module ? module.default : module
-        } catch (e) {
-            conn?.logger.error(e)
-            delete comandos[filename]
-        }
-    }))
+    const watching = watch(resolved, reload.bind(null, conn, resolved, filter))
+    watching.on('close', () => deletecomandosFolder(resolved, true))
+    watcher[resolved] = watching
 
-
-    const watching = watch(folder, reload.bind(null, conn, folder, comandosFilter))
-    watching.on('close', () => deletecomandosFolder(folder, true))
-    watcher[folder] = watching
-
-    return plugins
+    return comandos
 }
 
 function deletecomandosFolder(folder, isAlreadyClosed = false) {
@@ -45,28 +46,38 @@ function deletecomandosFolder(folder, isAlreadyClosed = false) {
     comandosFolders.splice(comandosFolders.indexOf(resolved), 1)
 }
 
-async function reload(conn, comandosFolder = comandosFolder, comandosFilter = comandosFilter, _ev, filename) {
-    if (comandosFilter(filename)) {
-        let dir = globalThis.__filename(join(pluginFolder, filename), true)
+async function reload(conn, folder = comandosFolder, filter = comandosFilter, _ev, filename) {
+    if (filter(filename)) {
+        let dir = globalThis.__filename(join(folder, filename), true)
+
         if (filename in comandos) {
-            if (existsSync(dir)) conn.logger.info(` updated comando - '${filename}'`)
+            if (existsSync(dir)) conn?.logger.info(`updated comando - '${filename}'`)
             else {
                 conn?.logger.warn(`deleted comando - '${filename}'`)
                 return delete comandos[filename]
             }
-        } else conn?.logger.info(`new comando - '${filename}'`)
+        } else {
+            conn?.logger.info(`new comando - '${filename}'`)
+        }
+
         let err = syntaxerror(readFileSync(dir), filename, {
             sourceType: 'module',
             allowAwaitOutsideFunction: true
         })
-        if (err) conn.logger.error(`syntax error while loading '${filename}'\n${format(err)}`)
-        else try {
-            const module = await importFile(globalThis.__filename(dir)).catch(console.error)
-            if (module) comandos[filename] = module
-        } catch (e) {
-            conn?.logger.error(`error require comandos '${filename}\n${format(e)}'`)
-        } finally {
-            comandos = Object.fromEntries(Object.entries(comandos).sort(([a], [b]) => a.localeCompare(b)))
+
+        if (err) {
+            conn?.logger.error(`syntax error while loading '${filename}'\n${format(err)}`)
+        } else {
+            try {
+                const module = await importFile(globalThis.__filename(dir)).catch(console.error)
+                if (module) comandos[filename] = module
+            } catch (e) {
+                conn?.logger.error(`error require comandos '${filename}\n${format(e)}'`)
+            } finally {
+                comandos = Object.fromEntries(
+                    Object.entries(comandos).sort(([a], [b]) => a.localeCompare(b))
+                )
+            }
         }
     }
 }
