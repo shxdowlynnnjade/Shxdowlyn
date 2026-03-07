a344d2d1f016701301092f55909c469bacb06e6a
shxdowlynnnjade <tvxuperlove@gmail.com>
2026-03-07 20:47:16 -0300

    Eliminar import.js

--- comandos/configuraciones/import.js
+++
@@ -1,8 +0,0 @@
-import manejador.js from './manejador.js'
-
-const WORKER_DIR = manejador.__dirname(import.meta.url, false)
-    module = manejador.__filename(module)
-    const module_ = await import(`${module}?id=${Date.now()}`)
-    const result = module_ && 'default' in module_ ? module_.default : module_
-    return result
-}
