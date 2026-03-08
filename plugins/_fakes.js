82183fa76287e5b5539258c975d50bf2b64ad80e
shxdowlynnnjade <tvxuperlove@gmail.com>
2026-03-08 15:07:20 -0300

    Actualizar _fakes.js

--- plugins/_fakes.js
+++ plugins/_fakes.js
@@ -7,26 +7,30 @@ const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
 
 var handler = m => m
 handler.all = async function (m) {
+
 global.canalIdM = ["120363404822730259@newsletter", "120363406529946290@newsletter"]
 global.canalNombreM = ["Shxdowlyn Channel", "Jade update ᕗ ᐛ"]
 global.channelRD = await getRandomChannel()
 
-global.d = new Date(new Date + 3600000)
+global.d = new Date(new Date() + 3600000)
 global.locale = 'es'
-global.dia = d.toLocaleDateString(locale, {weekday: 'long'})
-global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'})
-global.mes = d.toLocaleDateString('es', {month: 'long'})
-global.año = d.toLocaleDateString('es', {year: 'numeric'})
-global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
+
+global.dia = d.toLocaleDateString(locale, { weekday: 'long' })
+global.fecha = d.toLocaleDateString('es', { day: 'numeric', month: 'numeric', year: 'numeric' })
+global.mes = d.toLocaleDateString('es', { month: 'long' })
+global.año = d.toLocaleDateString('es', { year: 'numeric' })
+global.tiempo = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
 
 var canal = 'https://yotsuba-web.giize.com'
 var comunidad = 'https://yotsuba-web.giize.com'
-var git = 'hhttps://yotsuba-web.giize.com'
+var git = 'https://yotsuba-web.giize.com'
 var github = 'https://yotsuba-web.giize.com'
 var correo = 'https://whatsapp.com/channel/0029VbBkjlfLSmbWl3SH6737'
-global.redes = [canal, comunidad, git, github, correo].getRandom()
+
+global.redes = pickRandom([canal, comunidad, git, github, correo])
 
 global.nombre = m.pushName || 'Anónimo'
+
 global.packsticker = `⊹ ShxdowlynClyn
 ↳ Lyyynnie.club
 
@@ -37,10 +41,52 @@ global.packsticker2 = `⊹ 👑Bot:
 ⊹ ↳ @${botname}
 
 👑 Usuario:
-⊹ ↳ @${nombre}`}
+⊹ ↳ @${nombre}`
 
-global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD }}, "participant": "0@s.whatsapp.net" }
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: '', newsletterName: channelRD.name }, externalAdReply: { title: botname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnail: await (await fetch(icono)).buffer(), sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, mentionedJid: null }}
+global.fkontak = {
+  key: {
+    participants: "0@s.whatsapp.net",
+    remoteJid: "status@broadcast",
+    fromMe: false,
+    id: "Halo"
+  },
+  message: {
+    contactMessage: {
+      vcard: `BEGIN:VCARD
+VERSION:3.0
+N:Sy;Bot;;;
+FN:y
+item1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}
+item1.X-ABLabel:Ponsel
+END:VCARD`
+    }
+  },
+  participant: "0@s.whatsapp.net"
+}
+
+global.rcanal = {
+  contextInfo: {
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: channelRD.id,
+      serverMessageId: '',
+      newsletterName: channelRD.name
+    },
+    externalAdReply: {
+      title: botname,
+      body: dev,
+      mediaUrl: null,
+      description: null,
+      previewType: "PHOTO",
+      thumbnail: await (await fetch(icono)).buffer(),
+      sourceUrl: redes,
+      mediaType: 1,
+      renderLargerThumbnail: false
+    },
+    mentionedJid: null
+  }
+}
+
 }
 
 export default handler
@@ -55,4 +101,3 @@ let id = canalIdM[randomIndex]
 let name = canalNombreM[randomIndex]
 return { id, name }
 }
-
