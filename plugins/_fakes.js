61fc200355ade7cfb2a891cc245bdb2f35d8f7d3
shxdowlynnnjade <tvxuperlove@gmail.com>
2026-03-08 15:04:41 -0300

    Actualizar _fakes.js

--- plugins/_fakes.js
+++ plugins/_fakes.js
@@ -8,7 +8,7 @@ const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
 var handler = m => m
 handler.all = async function (m) {
 global.canalIdM = ["120363404822730259@newsletter", "120363406529946290@newsletter"]
-global.canalNombreM = ["Akina Wa Channel", "ᐛ ᕕ  𝐘𝐨𝐭𝐬𝐮𝐛𝐚 - 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 ᕗ ᐛ"]
+global.canalNombreM = ["Shxdowlyn Channel", "Jade update ᕗ ᐛ"]
 global.channelRD = await getRandomChannel()
 
 global.d = new Date(new Date + 3600000)
@@ -27,8 +27,17 @@ var correo = 'https://whatsapp.com/channel/0029VbBkjlfLSmbWl3SH6737'
 global.redes = [canal, comunidad, git, github, correo].getRandom()
 
 global.nombre = m.pushName || 'Anónimo'
-global.packsticker = ⊹ 🐬 Deymoon🧠 Club\n↳ Deymoon.club\n\n👹 Info:\n deymoon-club.vercel.app/
-global.packsticker2 = ⊹ 👑Bot:\n⊹ ↳ @${botname}\n\n👑 Usuario:\n⊹ ↳ @${nombre}
+global.packsticker = `⊹ ShxdowlynClyn
+↳ Lyyynnie.club
+
+🐢 Info:
+deymoon-club.vercel.app/`
+
+global.packsticker2 = `⊹ 👑Bot:
+⊹ ↳ @${botname}
+
+👑 Usuario:
+⊹ ↳ @${nombre}`}
 
 global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD }}, "participant": "0@s.whatsapp.net" }
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: '', newsletterName: channelRD.name }, externalAdReply: { title: botname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnail: await (await fetch(icono)).buffer(), sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, mentionedJid: null }}
